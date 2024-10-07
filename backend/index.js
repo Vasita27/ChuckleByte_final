41f876e26f2c89a3b0337baecfcce1d0a8c1bfaf
Vasita27 <147323404+Vasita27@users.noreply.github.com>
2024-10-07 06:43:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
   // Secret key for JWT
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://final-task-chucklebyte-qohm.vercel.app/"],
+    methods:["POST","GET"],
+    credentials:true
+));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

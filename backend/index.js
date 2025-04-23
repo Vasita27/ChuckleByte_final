4d9d76f953d8827ac4db50a9ee435bd5ac1f7519
Vasita27 <vasita.puppala@gmail.com>
2025-04-23 11:31:17 +0530

    minor change

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // CORS setup
 app.use(cors({
-  origin: ["https://final-task-chucklebyte-qohm.vercel.app"],
+  origin: ["*"],
   methods: ["POST", "GET"],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
